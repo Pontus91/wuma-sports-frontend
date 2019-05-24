6cdbd56886e9d3b3d7deb0eee080acf9f5e06620
Pontus Nilsson <p0nta_@hotmail.com>
2019-05-24 13:19:29 +0200

    Initial commit

---
+++ src/vy/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+  StyledMainWrapper,
+} from './StyledMainPage'
+
+const MainPage = () => {
+  return (
+    <StyledMainWrapper>Test 123</StyledMainWrapper>
+  )
+}
+
+export default MainPage;
