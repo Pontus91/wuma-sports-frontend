b76ad78dab315cfbe4d45c3b2febe9776a793c51
Pontus Nilsson <p0nta_@hotmail.com>
2019-06-03 15:12:17 +0200

    initial commit on redux first router

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import TestoneReducer from '../store/Test/TestoneReducer';
+import { connectRoutes } from 'redux-first-router';
+import { routePaths } from '../vy/index';
+
+const { reducer: location } = connectRoutes(routePaths)
 
 export default combineReducers({
+  location,
   testone: TestoneReducer
  });
---
+++ src/vy/index.js
@@ -0,0 +1,5 @@
+export const routePaths = {
+  'STARTPAGE': '/',
+  'MAINPAGE': '/main'
+}
+
