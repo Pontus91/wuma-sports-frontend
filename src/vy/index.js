126f8d42d74332893af99b5173f882e3f0b533a6
Pontus Nilsson <p0nta_@hotmail.com>
2019-06-06 10:25:45 +0200

    Changed for PR

--- src/vy/index.js
+++ src/vy/index.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import * as components from '../Components/components'
+import { connect } from 'react-redux'
+
 export const routePaths = {
   'HOME': '/',
-  'MAIN': '/main'
+  'MAIN': '/main',
+  'LOGIN': '/login'
 }
 
+const Views = ({ page }) => {
+  const Component = components[page]
+  return <Component />
+}
 
-
+const mapStateToProps = ({ page }) => ({ page })
+export default connect(mapStateToProps)(Views)
