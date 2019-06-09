d091a1b8dfbd542aa087f91b7e1122fb05a5f92d
Pontus Nilsson <p0nta_@hotmail.com>
2019-06-09 10:35:22 +0200

    Added google user ID and Button

--- src/vy/LoginPage/LoginPage.js
+++ src/vy/LoginPage/LoginPage.js
@@ -1,50 +1,22 @@
 import React from 'react'
-import Input from '../../Components/Input/Input'
-import Button from '../../Components/Button/Button'
 import Footer from '../../Components/Footer/Footer'
-import {
-  inputInfo,
-  LoginButtonInfo
-} from '../../staticData'
 import {
   StyledLoginWrapper,
   StyledLoginContainer,
   StyledLogin,
   StyledLoginHeader,
-  StyledLabel,
-  StyledFormWrapper,
   StyledButtonWrapper
 } from './StyledLoginPage'
 
 const LoginPage = () => {
-
-  /**
-   * Render login button
-   */
-  const renderButton = () => LoginButtonInfo.map(({ id, text }) => <Button key={id} text={text} />)
-
-  /**
-   * Render input
-   */
-  const renderInput = () => inputInfo.map(({ id, name, placeholder }) =>
-    <StyledFormWrapper key={id}>
-      <StyledLabel>
-        {name}
-      </StyledLabel>
-      <Input
-        placeholder={placeholder}
-      />
-    </StyledFormWrapper>)
-
 
   return (
     <StyledLoginWrapper>
       <StyledLoginContainer>
         <StyledLogin>
           <StyledLoginHeader>Login</StyledLoginHeader>
-            {renderInput()}
+          <div className="g-signin2" data-onsuccess="onSignIn"></div>
           <StyledButtonWrapper>
-            {renderButton()}
           </StyledButtonWrapper>
         </StyledLogin>
       </StyledLoginContainer>
