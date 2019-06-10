a21c22a814f40640b38ae0a7b6f042b7b5a5ffbc
Pontus Nilsson <p0nta_@hotmail.com>
2019-06-10 19:10:53 +0200

    Added : // to request

--- src/API/requests.js
+++ src/API/requests.js
@@ -1,32 +1,14 @@
 import axios from 'axios'
 
+  /**
+   * Sends token to backend with Axios
+   */
   const verifyToken = () => {
     const currentToken = localStorage.getItem('token')
     console.log(currentToken)
-    axios.post('localhost:3001/api/verifyToken', {
+    axios.post('http://localhost:3001/api/verifyToken', {
       "token": currentToken,
     })
   }
 
 export default verifyToken
-/*
-
-const config = {
-      headers: {'Authorization': "bearer" + token}
-    }
-  
-    const bodyParameters = {
-      key: "value"
-    }
-
-    Axios.post('',
-     bodyParameters,
-     config,
-     ).then((response) => {
-       console.log(response)
-     }).catch((error) => {
-       console.log(error)
-     })
-  }
-
-  */
