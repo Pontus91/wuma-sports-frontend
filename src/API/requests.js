197331a138d64ecb13a5a93e667a1663d734a545
Joel <joel.pedersen97@gmail.com>
2019-06-12 19:23:12 +0200

    token is now sent in header

--- src/API/requests.js
+++ src/API/requests.js
@@ -2,25 +2,25 @@ import axios from 'axios'
 const currentToken = localStorage.getItem('token')
 
 const headers = {
-  Bearer: currentToken,
+  "Bearer": currentToken,
 }
 
-  /**
-   * Sends token to backend with Axios
-   */
-  const verifyToken = () => {
-    axios.post('http://localhost:3001/api/verifyToken', {
-      "token": currentToken,
-    })
-  }
-
-  /**
-   * Gets user from our mongo database using Axios
-   */
-  const getUser = () => {
-    axios.get('http://localhost:3001/api/userDetails', null, {
-      headers
+/**
+ * Sends token to backend with Axios
+ */
+const verifyToken = () => {
+  axios.post('http://localhost:3001/api/verifyToken', {
+    "token": currentToken,
   })
+}
+
+/**
+ * Gets user from our mongo database using Axios
+ */
+const getUser = () => {
+  console.log(headers);
+
+  axios.get('http://localhost:3001/api/userDetails', { headers });
 }
 
 export {
